040b1f09cca9f02ec71202e7c4c6585588816f6a
SadatRiyad <sadatriyad@students.diu.ac>
2024-06-16 02:37:38 +0600

    jwt token implemented

--- index.js
+++ index.js
@@ -39,64 +39,106 @@ app.use(
   })
 );
 
-// verifyToken middleware
-const verifyToken = (req, res, next) => {
-  const token = req.cookies?.token;
-  console.log("value inside verifyToken", token);
-  if (!token) {
-    return res.status(401).send({ error: "Unauthorized" });
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      console.log(err);
-      return res.status(401).send({ error: "Unauthorized" });
-    }
-    console.log("value in the token", decoded);
-    req.user = decoded;
-    next();
-  });
-};
-
 // Routes
 app.get("/", (req, res) => {
   res.send("BB-Matrimony server is running");
 });
 
 async function run() {
-  // Database Collections
-  const db = client.db("BB-MatrimonyDB");
-  const UsersCollection = db.collection("Users");
-  const BiodatasCollection = db.collection("Biodatas");
-  const MarriagesCollection = db.collection("Marriages");
+  try {
+    // await client.connect();
+    // Database Collections
+    const db = client.db("BB-MatrimonyDB");
+    const UsersCollection = db.collection("Users");
+    const BiodatasCollection = db.collection("Biodatas");
+    const MarriagesCollection = db.collection("Marriages");
 
+    // verifyToken 
+    const verifyToken = (req, res, next) => {
+      const token = req.cookies?.token;
+      console.log("value inside verifyToken", token);
+      if (!token) {
+        return res.status(401).send({ error: "Unauthorized" });
+      }
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          console.log(err);
+          return res.status(401).send({ error: "Unauthorized" });
+        }
+        console.log("value in the token", decoded);
+        req.user = decoded;
+        next();
+      });
+    };
+
+    // use verify admin after verifyToken
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await UsersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+       // users related api
+       app.get('/users', async (req, res) => {
+      //  app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+        const result = await UsersCollection.find().toArray();
+        res.send(result);
+      });
   
-// Function to generate biodata ID
-const generateBiodataID = async (biodataType) => {
-  try {
-    // Example: Fetch the last biodata ID from the database and increment it
-    // Assuming you have a counter collection named "counters" with a document for biodataID
-    const counter = await client
-      .db("BB-MatrimonyDB")
-      .collection("Biodatas")
-      .findOneAndUpdate(
-        { _id: "biodataID" },
-        { $inc: { sequence_value: 1 } },
-        { returnDocument: "after", upsert: true }
-      );
+      app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+  
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: 'forbidden access' })
+        }
+  
+        const query = { email: email };
+        const user = await UsersCollection.findOne(query);
+        let admin = false;
+        if (user) {
+          admin = user?.role === 'admin';
+        }
+        res.send({ admin });
+      })
+  
+      app.post('/users', async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await UsersCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: 'user already exists', insertedId: null })
+        }
+        const result = await UsersCollection.insertOne(user);
+        res.send(result);
+      });
+  
+      app.patch('/users/admin/:id', async (req, res) => {
+      // app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: 'admin'
+          }
+        }
+        const result = await UsersCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      })
+  
+      app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await UsersCollection.deleteOne(query);
+        res.send(result);
+      })
 
-    // Format the ID based on biodataType
-    const sequenceValue = counter.value.sequence_value.toString().padStart(3, '0'); // Assuming a 3-digit sequence
-    const prefix = biodataType === 'Male' ? 'BBM' : 'BBF';
-    const biodataID = `${prefix}-${sequenceValue}`;
+      
 
-    return biodataID;
-  } catch (error) {
-    console.error("Error generating biodata ID:", error);
-    throw error;
-  }
-};
-
-  try {
     // Get all the data from the collection
     app.get("/biodatas", async (req, res) => {
       const data = BiodatasCollection.find().sort({ datePosted: -1 });
@@ -104,6 +146,13 @@ const generateBiodataID = async (biodataType) => {
       res.send(result);
     });
 
+    // Post biodata
+    app.post("/biodata", async (req, res) => {
+      const biodata = req.body;
+      const result = await BiodatasCollection.insertOne(biodata);
+      res.send(result);
+    });
+    
     // put biodata using email
     app.put("/biodata/:email", async (req, res) => {
       const email = req.params.email;
@@ -149,6 +198,13 @@ const generateBiodataID = async (biodataType) => {
       }
     });
 
+    // get biodataID everytime +1
+    app.get("/biodataID", async (req, res) => {
+      const data = await BiodatasCollection.find().sort({ biodataID: -1 }).limit(1).toArray();
+      const biodataID = data[0]?.biodataID + 1 || 1;
+      res.send({ biodataID });
+    });
+
     //creating Token
     app.post("/jwt", async (req, res) => {
       const user = req.body;
@@ -156,8 +212,7 @@ const generateBiodataID = async (biodataType) => {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "7d",
       });
-
-      res.cookie("token", token, cookieOptions).send({ success: true });
+      res.cookie("token", token, cookieOptions).send({token});
     });
 
     //clearing Token
